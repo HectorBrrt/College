644f751be7692ed65beccb4db5e5735f6a7a22c3
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-05-12 17:14:13 -0300

    Update 03.c

--- AssignmentsList_2/03.c
+++ AssignmentsList_2/03.c
@@ -1,3 +1,7 @@
+/* Faça um algoritmo que leia o tempo de duração de um evento em uma fábrica expressa em segundos e
+ * mostre-o expresso em horas, minutos e segundos. 
+ */
+
 #include <stdio.h>
 
 int main()
