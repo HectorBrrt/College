9b19246d3052ad9d4a0c90845a16697212bdf8e2
HectorBrrt <105394865+HectorBrrt@users.noreply.github.com>
2022-05-12 17:02:05 -0300

    Create 10.c

---
+++ AssignmentsList_2/10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, n3, n4, n5, somai;
+    n1 = n2 = n3 = n4 = n5 = somai = 0;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%d", &n2);
+
+    printf("Digite o terceiro valor: ");
+    scanf("%d", &n3);
+
+    printf("Digite o quarto valor: ");
+    scanf("%d", &n4);
+
+    printf("Digite o quinto valor: ");
+    scanf("%d", &n5);
+
+    somai = n1*(n1%2) + n2*(n2%2) + n3*(n3%2) + n4*(n4%2) + n5*(n5%2);
+
+    printf("\nA soma dos numeros pares sera de: %d", somai);
+    return 0;
+}
