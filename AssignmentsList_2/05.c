000a7da519543193962c4906ce9b672a2c6239a2
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-05-12 17:18:05 -0300

    Update 05.c

--- AssignmentsList_2/05.c
+++ AssignmentsList_2/05.c
@@ -1,3 +1,10 @@
+/* Uma fábrica de camisetas produz os tamanhos pequeno, médio e grande, cada uma sendo vendida 
+ * respectivamente por 10, 12 e 15 reais. Construa um algoritmo em que o usuário forneça a quantidade de
+ * camisetas pequenas, médias e grandes referentes a uma venda, e a máquina informe quanto será o valor
+ * arrecadado. 
+ */
+
+
 #include <stdio.h>
 
 int main() {
