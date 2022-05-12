fdf799b3902965eaa265723a5e83ec880acca600
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-05-12 17:20:03 -0300

    Update 07.c

--- AssignmentsList_2/07.c
+++ AssignmentsList_2/07.c
@@ -1,3 +1,9 @@
+/* Três amigos, Carlos, André e Felipe. decidiram rachar igualmente a conta de um restaurante. Faça um
+ * algoritmo para ler o valor total da conta e imprimir quanto cada um deve pagar, mas faça com que Carlos
+ * e André não paguem centavos. Ex: uma conta de R$101,53 resulta em R$33,00 para Carlos, R$33,00 para
+ * André e R$35,53 para Felipe.
+ */
+
 #include <stdio.h>
 
 int main()
