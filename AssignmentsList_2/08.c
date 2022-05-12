c0671777a2c0e3af5c43f01b2a4651306611324c
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-05-12 17:21:19 -0300

    Update 08.c

--- AssignmentsList_2/08.c
+++ AssignmentsList_2/08.c
@@ -1,3 +1,10 @@
+/* A lanchonete Gosto vende apenas um tipo de sanduíche, cujo recheio inclui duas fatias de queijo, uma
+ * fatia de presunto e uma rodela de hambúrguer. Sabendo que cada fatia de queijo ou presunto pesa 50
+ * gramas, e que a rodela de hambúrguer pesa 100 gramas, faça um algoritmo em que o dono forneça a
+ * quantidade de sanduíches a fazer, e a máquina informe as quantidades (em quilos) de queijo, presunto e
+ * carne necessários para compra.
+ */
+
 /* RecheioSanduiche = 2 fatias de queijo + 1 fatia de presunto + 1 rodela de hamburguer.
  * queijo = presunto = 50 gramas
  * rodelo de hamburguer = 100 gramas
